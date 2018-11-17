c06cb52f63c9302658803dc78bb5b19fbeb5534b
Didier DONSEZ <didier.donsez@gmail.com>
2018-11-17 16:18:55 +0100

    improve

--- generate.sh
+++ generate.sh
@@ -7,6 +7,11 @@ wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagge
 
 java -jar swagger-codegen-cli.jar help
 java -jar swagger-codegen-cli.jar langs
-java -jar swagger-codegen-cli.jar generate -i api.swagger.json  -l bash -o bash
+
 java -jar swagger-codegen-cli.jar generate -i api.swagger.json  -l html2 -o html2
+# (cd html2; open index.html)
+
 # java -jar swagger-codegen-cli.jar generate -i api.swagger.json  -l javascript -o javascript
+# (cd javascript; npm install)
+
+java -jar swagger-codegen-cli.jar generate -i api.swagger.json  -l bash -o bash
