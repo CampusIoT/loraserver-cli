c9aaea4255f05f193369cc63953398892d488691
Didier DONSEZ <didier.donsez@gmail.com>
2018-11-17 19:39:42 +0100

    update

--- generate.sh
+++ generate.sh
@@ -5,6 +5,9 @@ wget $SWAGGER_DOC --no-check-certificate
 
 wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O swagger-codegen-cli.jar
 
+sudo apt-get update
+sudo apt-get install default-jre
+
 java -jar swagger-codegen-cli.jar help
 java -jar swagger-codegen-cli.jar langs
 
@@ -15,3 +18,16 @@ java -jar swagger-codegen-cli.jar generate -i api.swagger.json  -l html2 -o html
 # (cd javascript; npm install)
 
 java -jar swagger-codegen-cli.jar generate -i api.swagger.json  -l bash -o bash
+
+cd bash/
+chmod +x client.sh
+./client.sh --help
+./client.sh --about
+./client.sh --insecure --host https://lora.campusiot.imag.fr/api list --help
+
+
+
+
+$ output/petstore-cli --host http://petstore.swagger.io --content-type json --dry-run addPet id:=891 name==lucky status==available
+
+curl -sS --tlsv1.2 -H 'Content-type: application/json' -X POST -d '{"name": "lucky", "status": "available", "id": 891}' "http://petstore.swagger.io/v2/pet"
